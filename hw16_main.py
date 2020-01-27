7693da3b9bb5b9dc0e877aafa47027ab84aec4ed
tihhor <tihhor@gmail.com>
2020-01-27 20:56:37 +0200

    homework 17-1

--- hw16_main.py
+++ hw16_main.py
@@ -45,23 +45,11 @@ def results():
 
 @app.route('/contact/')
 def contacts():
-    # где то взяли данные
-    developer_name = 'Leo'
-    # Контекст name=developer_name - те данные, которые мы передаем из view в шаблон
-    # context = {'name': developer_name}
-    # Словарь контекста context
-    # return render_template('contacts.html', context=context)
-    return render_template('contact.html', name=developer_name, creation_date='16.01.2020')
+
+    return render_template('contact.html')
 
 
 
-@app.route('/run/', methods=['GET'])
-def run_get():
-    with open('main.txt', 'r') as f:
-        text = f.read()
-    return render_template('form.html', text=text)
-    # with open('main.txt', 'a') as f:
-    #     f.write('hello')
 
 
 if __name__ == "__main__":
