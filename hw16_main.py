f368f583499ed9f97f123fce318e1bdd39c8e09d
tihhor <tihhor@gmail.com>
2020-01-27 22:10:41 +0200

    homework 17-2

--- hw16_main.py
+++ hw16_main.py
@@ -4,15 +4,25 @@ import pprint
 import time
 import json
 from hhrequest import hh_request
+import sqlite3
+
+# Подключение к базе данных
+conn = sqlite3.connect('hh_requests.sqlite', check_same_thread=False)
+
+# Создаем курсор
+cursor = conn.cursor()
+
+# cursor.execute('SELECT * from region')
+#
+# result = cursor.fetchall()
+# print(result)
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def main():
     msg = 'Домашнее задание №16'
     return render_template('index.html', message=msg)
-
 
 
 @app.route("/index/")
@@ -30,6 +40,21 @@ def request_get():
 def request_post():
     data = request.form['what']
     request_result = hh_request(data)
+    cursor.execute("insert into requests (request_text, vacancies_total, average_salary) "
+                   "VALUES (?, ?, ?)", (request_result['request_text'],
+                                        request_result['vacancies_total'],
+                                        request_result['average_salary']))
+    conn.commit()
+    cursor.execute('SELECT MAX(id) from requests')
+    result = cursor.fetchall()
+    curr_id = result[0][0]
+    for item in request_result['key_skills']:
+        cursor.execute("insert into key_skills (request_id, key_skill_name, key_skill_total) "
+                       "VALUES (?, ?, ?)", (curr_id,
+                                            item[0],
+                                            item[1]))
+    conn.commit()
+
 
     return render_template('results.html', data=request_result)
 
